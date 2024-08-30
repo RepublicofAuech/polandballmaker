9ad3a29b654e97f586363b9c92a11baa27ea9d3d
RepublicofAuech <168240154+RepublicofAuech@users.noreply.github.com>
2024-08-30 23:14:45 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,71 +28,119 @@ async def on_ready():
     print(f'Logged in as {bot.user}!')
 
 # Dictionaries to map inputs to corresponding images
-FLAG_COUNTRY = {
-    '日本': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/japanballpbmaker.png?raw=true',
-    'ブラジル': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/brazilballpbmaker.png?raw=true',
-    'オーストラリア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/australiaballpbmaker.png?raw=true',
-    'カナダ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/canadaballpbmaker.png?raw=true',
-    '中国': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/chinaballpbmaker.png?raw=true',
+
+ENEUROPE_COUNTRY = {
+    'ポーランド': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/polandballpbmaker1.png?raw=true',
+    'ロシア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/russiaballpbmaker.png?raw=true'
+}
+WSEUROPE_COUNTRY = {
     'フランス': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/franceballpbmaker.png?raw=true',
     'ドイツ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/germanyballpbmaker.png?raw=true',
     'イタリア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/italyballpbmaker.png?raw=true',
-    'ニュージーランド': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/newzealandballpbmaker.png?raw=true',
-    'ポーランド': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/polandballpbmaker1.png?raw=true',
-    '台湾': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/taiwanballpbmaker.png?raw=true',
     'イギリス': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/ukballpbmaker.png?raw=true',
-    'アメリカ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/usaballpbmaker.png?raw=true',
-    'アルゼンチン': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/argentinaballpbmaker.png?raw=true',
-    'EU': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/euballpbmaker.png?raw=true',
-    'インド': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/indiaballpbmaker.png?raw=true',
-    'インドネシア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/indonesiaballpbmaker.png?raw=true',
-    'メキシコ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/mexicoballpbmaker.png?raw=true',
-    'ロシア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/russiaballpbmaker.png?raw=true',
-    'サウジアラビア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/saudiarabiaballpbmaker.png?raw=true',
-    'シンガポール': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/singaporeballpbmaker.png?raw=true',
-    '南アフリカ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/southafricaballpbmaker.png?raw=true',
-    '韓国': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/southkoreaballpbmaker.png?raw=true',
+    'トルコ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/turkeyballpbmaker.png?raw=true'
+}
+
+MIDEASCAUCASUS_COUNTRY = {
     'トルコ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/turkeyballpbmaker.png?raw=true',
-    'アルジェリア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/algeriapbmaker.png?raw=true',
+    'エジプト': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/egyptpbmaker.png?raw=true',
+    'サウジアラビア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/saudiarabiaballpbmaker.png?raw=true'
+}
+
+ESEASIA_COUNTRY = {
+    '日本': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/japanballpbmaker.png?raw=true',
+    '中国': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/chinaballpbmaker.png?raw=true',
+    '台湾': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/taiwanballpbmaker.png?raw=true',
+
+    'インドネシア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/indonesiaballpbmaker.png?raw=true',
+    'シンガポール': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/singaporeballpbmaker.png?raw=true',
+    '韓国': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/southkoreaballpbmaker.png?raw=true',
+}
+
+CSASIA_COUNTRY = {
+     'インド': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/indiaballpbmaker.png?raw=true'
+
+}
+
+NCAMERICA_COUNTRY = {
+    'アメリカ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/usaballpbmaker.png?raw=true',
+    'メキシコ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/mexicoballpbmaker.png?raw=true',
+    'カナダ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/canadaballpbmaker.png?raw=true'
+
+}
+
+SAMERICA_COUNTRY = {
+    'アルゼンチン': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/argentinaballpbmaker.png?raw=true',
+    'ブラジル': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/brazilballpbmaker.png?raw=true'
+
+}
+
+NWAFRICA_COUNTRY = {
     'エジプト': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/egyptpbmaker.png?raw=true',
     'リビア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/libyapbmaker.png?raw=true',
     'モロッコ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/moroccopbmaker.png?raw=true',
-    'チュニジア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/tunisiapbmaker.png?raw=true'
+    'チュニジア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/tunisiapbmaker.png?raw=true',
+    'アルジェリア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/algeriapbmaker.png?raw=true'
+
 }
 
-FLAG_PREFECTURE = {
+CSAFRICA_COUNTRY = {
+    '南アフリカ': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/southafricaballpbmaker.png?raw=true',
+
+}
+
+OCEANIA_COUNTRY = {
+    'ニュージーランド': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/newzealandballpbmaker.png?raw=true',
+    'オーストラリア': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/australiaballpbmaker.png?raw=true',
+
+}
+
+
+TOHTOCHU_PREF = {
     '愛知県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/aichiballpbmaker.png?raw=true',
     '千葉県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/chibapbmaker.png?raw=true',
-    '広島県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/hiroshimapbmaker.png?raw=true',
     '北海道': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/hokkaidopbmaker.png?raw=true',
-    '福岡県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/fukuokapbmaker.png?raw=true',
-    '兵庫県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/hyogopbmaker.png?raw=true',
     '茨城県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/ibarakipbmaker.png?raw=true',
     '神奈川県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/kanagawaballpbmaker.png?raw=true',
-    '京都府': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/kyotopbmaker.png?raw=true',
     '宮城県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/miyagipbmaker.png?raw=true',
     '新潟県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/niigataballpbmaker.png?raw=true',
-    '大阪府': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/osakaprefballpbmaker.png?raw=true',
     '埼玉県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/saitamaballpbmaker.png?raw=true',
     '東京都': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/tokyoballpbmaker.png?raw=true',
     '東京都シンボルver': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/tokyoballpbmaker2.png?raw=true',
-    '熊本県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/kumamotopbmaker.png?raw=true',
-    '三重県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/miepbmaker.png?raw=true',
-    '岡山県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/okayamapbmaker.png?raw=true',
     '静岡県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/shizuokapbmaker.png?raw=true',
     '長野県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/naganopbmaker.png?raw=true',
     '岐阜県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/gifupbmaker.png?raw=true',
-    '愛媛県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/ehimepbmaker.png?raw=true',
     '福島県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/fukushimapbmaker.png?raw=true'
  }
 
-CORE_CITY = {
-    '鹿児島市': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/kagoshimaballpbmaker.png?raw=true',
+KINTOKYU_PREF = {
+    '広島県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/hiroshimapbmaker.png?raw=true',
+    '兵庫県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/hyogopbmaker.png?raw=true',
+    '京都府': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/kyotopbmaker.png?raw=true',
+    '福岡県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/fukuokapbmaker.png?raw=true',
+    '大阪府': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/osakaprefballpbmaker.png?raw=true',
+    '熊本県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/kumamotopbmaker.png?raw=true',
+    '三重県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/miepbmaker.png?raw=true',
+    '愛媛県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/ehimepbmaker.png?raw=true',
+    '岡山県': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/okayamapbmaker.png?raw=true'
+}
+
+TOHTOKAN_CORE = {
+    '宇都宮市': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/utsunomiyaballpbmaker.png?raw=true'
+
+}
+
+CHUTOKIN_CORE = {
     '姫路市': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/himejicitypbmaker.png?raw=true',
     '金沢市': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/kanazawaballpbmaker.png?raw=true',
-    '宇都宮市': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/utsunomiyaballpbmaker.png?raw=true',
-    '大分市': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/oitacitypbmaker.png?raw=true',
     '豊田市': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/toyotapbmaker.png?raw=true',
+
+}
+
+
+CHUTOKYU_CORE = {
+    '鹿児島市': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/kagoshimaballpbmaker.png?raw=true',
+    '大分市': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/oitacitypbmaker.png?raw=true',
     '松山市': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/matsuyamapbmaker.png?raw=true'
 }
 
@@ -106,6 +154,14 @@ ORDINANCE_CITY = {
     '横浜市': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/yokohamapbmaker.png?raw=true'
 }
 
+OTHERS = {
+    'EU': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/euballpbmaker.png?raw=true',
+}
+
+NONE = {
+    'なし': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/invisibleimage.png?raw=true'
+}
+
 EXPRESSION_IMAGES = {
     '普通': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/eyes/normaleyes.png?raw=true',
     '楽しい': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/eyes/happyeyes.png?raw=true',
@@ -113,7 +169,8 @@ EXPRESSION_IMAGES = {
     '悲しい': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/eyes/sadeyes.png?raw=true',
     '号泣': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/eyes/cryingeyes.png?raw=true',
     '真面目': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/eyes/kindaserious.png?raw=true',
-    '怒り': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/eyes/angryeyes.png?raw=true'
+    '怒り': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/eyes/angryeyes.png?raw=true',
+    'なし': 'https://github.com/RepublicofAuech/polandballmaker/blob/main/flags/invisibleimage.png?raw=true'
  }
 
 POSITION_COMMANDS = {
@@ -129,17 +186,47 @@ POSITION_COMMANDS = {
 }
 
 CATEGORY_FLAGS = {
-    '国': FLAG_COUNTRY,
-    '都道府県': FLAG_PREFECTURE,
+    '西・南ヨーロッパの国々': WSEUROPE_COUNTRY,
+    '東・北ヨーロッパの国々': ENEUROPE_COUNTRY,
+    '東・東南アジアの国々': ESEASIA_COUNTRY,
+    '中東・コーカサスの国々': MIDEASCAUCASUS_COUNTRY,
+    '中央・南アジアの国々': CSASIA_COUNTRY,
+    '北中米の国々': NCAMERICA_COUNTRY,
+    '南米の国々': SAMERICA_COUNTRY,
+    '北・西アフリカの国々': NWAFRICA_COUNTRY,
+    '中央・南アフリカの国々': CSAFRICA_COUNTRY,
+    'オセアニアの国々:': OCEANIA_COUNTRY,
+    '都道府県（東北から中部）': TOHTOCHU_PREF,
+    '都道府県（近畿から九州）': KINTOKYU_PREF,
     '政令市': ORDINANCE_CITY,
-    '中核市': CORE_CITY
+    '中核市（東北から関東）': TOHTOKAN_CORE,
+    '中核市（中部から近畿）': CHUTOKIN_CORE,
+    '中核市（中国から九州）': CHUTOKYU_CORE,
+    'その他': OTHERS,
+    'なし': NONE
 }
 
 CATEGORY_CHOICES = [
-    app_commands.Choice(name='国', value='国'),
-    app_commands.Choice(name='都道府県', value='都道府県'),
+    app_commands.Choice(name='西・南ヨーロッパの国々', value='西・南ヨーロッパの国々'),
+    app_commands.Choice(name='東・北ヨーロッパの国々', value='東・北ヨーロッパの国々'),
+    app_commands.Choice(name='中東・コーカサスの国々', value='中東・コーカサスの国々'),
+    app_commands.Choice(name='東・東南アジアの国々', value='東・東南アジアの国々'),
+    app_commands.Choice(name='中央・南アジアの国々', value='中央・南アジアの国々'),
+    app_commands.Choice(name='北中米の国々', value='北中米の国々'),
+    app_commands.Choice(name='カリブ海の国々', value='カリブ海の国々'),
+    app_commands.Choice(name='南米の国々', value='南米の国々'),
+    app_commands.Choice(name='北・西アフリカの国々', value='北・西アフリカの国々'),
+    app_commands.Choice(name='中央・南アフリカの国々', value='中央・南アフリカの国々'),
+    app_commands.Choice(name='東アフリカの国々', value='東アフリカの国々'),
+    app_commands.Choice(name='オセアニアの国々', value='オセアニアの国々'),
+    app_commands.Choice(name='都道府県（東北から中部）', value='都道府県（東北から中部）'),
+    app_commands.Choice(name='都道府県（近畿から九州）', value='都道府県（近畿から九州）'),
     app_commands.Choice(name='政令市', value='政令市'),
-    app_commands.Choice(name='中核市', value='中核市'),
+    app_commands.Choice(name='中核市（東北から関東）', value='中核市（東北から関東）'),
+    app_commands.Choice(name='中核市（中部から近畿）', value='中核市（中部から近畿）'),
+    app_commands.Choice(name='中核市（中国から九州）', value='中核市（中国から九州）'),
+    app_commands.Choice(name='その他', value='その他'),
+    app_commands.Choice(name='なし', value='なし')
 ]
 
 EXPRESSION_CHOICES = [
@@ -149,7 +236,8 @@ EXPRESSION_CHOICES = [
     app_commands.Choice(name='悲しい', value='悲しい'),
     app_commands.Choice(name='号泣', value='号泣'),
     app_commands.Choice(name='真面目', value='真面目'),
-    app_commands.Choice(name='怒り', value='怒り')
+    app_commands.Choice(name='怒り', value='怒り'),
+    app_commands.Choice(name='なし', value='なし')
 ]
 
 POSITION_CHOICES = [app_commands.Choice(name=pos, value=pos) for pos in POSITION_COMMANDS.keys()]
@@ -166,12 +254,23 @@ async def fetch_image(url):
         async with session.get(url) as response:
             if response.status == 200:
                 data = io.BytesIO(await response.read())
-                return Image.open(data)
+                image = Image.open(data)
+                return image.convert('RGBA')
             else:
+                print(f"Error fetching image: HTTP {response.status}")
                 return None
 
 # Function to merge the flag and expression images with more offset for each predefined position
 def merge_images(flag_img, expression_img, position):
+    if expression_img is None:
+        print("No expression image provided.")
+        # Optionally, you can handle this case differently, such as using a default image or skipping this step
+        return flag_img
+
+    # Convert expression_img to RGBA if not already
+    if expression_img.mode != 'RGBA':
+        expression_img = expression_img.convert('RGBA')
+
     # Resize the expression image to 100% of the flag's size
     new_width = int(flag_img.width * 1)
     new_height = int(flag_img.height * 1)
@@ -214,32 +313,32 @@ def merge_images(flag_img, expression_img, position):
 @app_commands.autocomplete(country=get_country_choices)
 async def pb_maker(interaction: discord.Interaction, 
                    category: app_commands.Choice[str], 
-                   country: str,  # Changed to a regular string annotation
+                   country: str, 
                    expression: app_commands.Choice[str], 
                    position: app_commands.Choice[str]):
 
-    # Fetch the corresponding image URLs
-    flags = CATEGORY_FLAGS.get(category.value, {})
-    flag_image = await fetch_image(flags.get(country, None))  # Use country directly
-    expression_image = await fetch_image(EXPRESSION_IMAGES.get(expression.value, None))
-    position_output = POSITION_COMMANDS.get(position.value, '位置設定が見つかりません')
+    flag_url = CATEGORY_FLAGS[category.value].get(country, None)
+    expression_url = EXPRESSION_IMAGES.get(expression.value, None)
+    
+    if flag_url is None:
+        await interaction.response.send_message("指定された国や地域の画像が見つかりませんでした。")
+        return
+    
+    flag_img = await fetch_image(flag_url)
+    expression_img = await fetch_image(expression_url)
 
-    if flag_image and expression_image:
-        # Merge the images
-        merged_image = merge_images(flag_image, expression_image, position.value)
+    if flag_img is None:
+        await interaction.response.send_message("旗の画像を取得できませんでした。")
+        return
 
-        # Save the merged image to a bytes buffer
-        buffer = io.BytesIO()
-        merged_image.save(buffer, format='PNG')
-        buffer.seek(0)
-
-        # Send the merged image
-        await interaction.response.send_message(
-            content=f"ポーランドボールの作成に成功しました",
-            file=discord.File(buffer, filename='pbmakerball.png')
-        )
-    else:
-        await interaction.response.send_message("画像の取得に失敗しました")
+    combined_img = merge_images(flag_img, expression_img, position.value)
+    
+    # Save and send the image (this part will need to be implemented based on how you want to handle it)
+    with io.BytesIO() as output:
+        combined_img.save(output, format='PNG')
+        output.seek(0)
+        file = discord.File(output, filename='polandball.png')
+        await interaction.response.send_message(file=file)
 
 thread = threading.Thread(target=run_flask)
 thread.start()
